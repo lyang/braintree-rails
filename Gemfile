c40d7f66508ff9b57f0e9e9f6200b0a61a0619f1
Lin Yang <github@linyang.me>
2012-09-06 22:44:48 -0700

    Adding braintree config for integration tests

--- Gemfile
+++ Gemfile
@@ -1,1 +1,2 @@
 source 'http://rubygems.org'
+gem "braintree", "~> 2.16.0"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,3 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "lib", "test_env"))
+TEST_PATH = File.expand_path(File.dirname(__FILE__))
 require "braintree-rails"
