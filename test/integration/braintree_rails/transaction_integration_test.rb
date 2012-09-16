2881fbb2bd8ab976925e6cf2b948afcffdf3f574
Lin Yang <github@linyang.me>
2012-09-15 18:07:57 -0700

    Adding one more integration test

--- test/integration/braintree_rails/transaction_integration_test.rb
+++ test/integration/braintree_rails/transaction_integration_test.rb
@@ -11,7 +11,7 @@ describe 'Transaction Integration' do
     customer = BraintreeRails::Customer.new(braintree_customer)
     credit_card = customer.credit_cards.first
 
-    [{:amount => 10.00, :customer => customer}, {:amount => 20.00, :customer => customer, :credit_card => credit_card}].each do |params|
+    [{:amount => rand(1..10), :customer => customer}, {:amount => rand(1..10), :customer => customer, :credit_card => credit_card}].each do |params|
       transaction = BraintreeRails::Transaction.create!(params)
       transaction.persisted?.must_equal true
       transaction.amount.must_equal params[:amount]
@@ -29,9 +29,22 @@ describe 'Transaction Integration' do
     braintree_customer = Braintree::Customer.create!(:id => 'customer_id', :first_name => 'Brain', :last_name => 'Tree', :credit_card => credit_card_hash)
     customer = BraintreeRails::Customer.new(braintree_customer)
     credit_card = customer.credit_cards.first
-    transaction = BraintreeRails::Transaction.create!({:amount => 15.00, :customer => customer})
+    transaction = BraintreeRails::Transaction.create!(:amount => rand(1..10), :customer => customer)
     
     customer.transactions.count.must_equal 1
     credit_card.transactions.count.must_equal 1
+  end
+
+  it 'should be able to capture braintree api errors' do
+    braintree_customer = Braintree::Customer.create!(:id => 'customer_id', :first_name => 'Brain', :last_name => 'Tree', :credit_card => credit_card_hash)
+    customer = BraintreeRails::Customer.new(braintree_customer)
+    credit_card = customer.credit_cards.first
+    transaction = BraintreeRails::Transaction.create!(:amount => rand(1..10), :customer => customer)
+
+    transaction.void!
+    transaction.submit_for_settlement.must_equal false
+    transaction.errors[:base].wont_be :blank?
+
+    lambda{transaction.submit_for_settlement!}.must_raise Braintree::ValidationsFailed
   end
 end
