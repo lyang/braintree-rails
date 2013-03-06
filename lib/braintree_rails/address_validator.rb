9870c9ba6ee99e55265cec307a8f131dccb6695a
Lin Yang <github@linyang.me>
2013-03-06 09:26:07 -0800

    Make some validations configurable

--- lib/braintree_rails/address_validator.rb
+++ lib/braintree_rails/address_validator.rb
@@ -1,11 +1,19 @@
 module BraintreeRails
   class AddressValidator < ActiveModel::Validator
+    def self.validate_postal_code?(address)
+      true
+    end
+
+    def self.validate_street_address?(address)
+      true
+    end
+
     def setup(klass)
       klass.class_eval do
         validates :first_name, :last_name, :company, :street_address, :extended_address, :locality, :region, :length => {:maximum => 255}
         validates :country_code_alpha2, :allow_blank => true, :inclusion => { :in => Braintree::Address::CountryNames.map {|country| country[1]} }
-        validates :postal_code, :street_address, :presence => true
-        validates :postal_code, :format => { :with => /\A[- a-z0-9]+\z/i}
+        validates :street_address, :presence => true, :if => Proc.new { |address| AddressValidator.validate_street_address?(address) }
+        validates :postal_code, :presence => true, :format => { :with => /\A[- a-z0-9]+\z/i}, :if => Proc.new { |address| AddressValidator.validate_postal_code?(address) }
       end
     end
 
--- lib/braintree_rails/credit_card_validator.rb
+++ lib/braintree_rails/credit_card_validator.rb
@@ -1,26 +1,42 @@
 module BraintreeRails
   class CreditCardValidator < ActiveModel::Validator
+    def self.validate_number?(credit_card)
+      credit_card.new_record?
+    end
+
+    def self.validate_cvv?(credit_card)
+      credit_card.new_record?
+    end
+
+    def self.validate_expiration_date?(credit_card)
+      true
+    end
+
+    def self.validate_billing_address?(credit_card)
+      true
+    end
+
     def setup(klass)
       klass.class_eval do
-        with_options :presence => true, :if => :new_record? do |credit_card|
-          credit_card.validates :customer_id, :length => {:maximum => 36}
-          credit_card.validates :number, :numericality => { :only_integer => true }, :length => {:minimum => 12, :maximum => 19}, 'braintree_rails/luhn_10' => true
-          credit_card.validates :cvv, :numericality => { :only_integer => true, :greater_than_or_equal_to => 100, :less_than_or_equal_to => 9999 }
-        end
+        validates :customer_id, :length => {:maximum => 36}, :presence => true, :if => :new_record?
         validates :cardholder_name, :length => {:maximum => 255}
-        validates :expiration_month, :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 12 }
-        validates :expiration_year,  :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1976, :less_than_or_equal_to => 2200 }
-        validates :billing_address, :presence => true
+        validates :number, :numericality => { :only_integer => true }, :length => {:minimum => 12, :maximum => 19}, 'braintree_rails/luhn_10' => true, :presence => true, :if => Proc.new { |credit_card| CreditCardValidator.validate_number?(credit_card) }
+        validates :cvv, :numericality => { :only_integer => true, :greater_than_or_equal_to => 100, :less_than_or_equal_to => 9999 }, :presence => true, :if => Proc.new { |credit_card| CreditCardValidator.validate_cvv?(credit_card) }
+        validates :expiration_month, :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 12 }, :if => Proc.new { |credit_card| CreditCardValidator.validate_expiration_date?(credit_card) }
+        validates :expiration_year,  :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1976, :less_than_or_equal_to => 2200 }, :if => Proc.new { |credit_card| CreditCardValidator.validate_expiration_date?(credit_card) }
       end
     end
 
     def validate(credit_card)
-      validate_billing_address(credit_card)
+      have_valid_billing_address(credit_card)
     end
 
-    def validate_billing_address(credit_card)
-      if credit_card.billing_address && credit_card.billing_address.invalid?
-        credit_card.errors.add(:billing_address, "is not valid. #{credit_card.billing_address.errors.full_messages.join("\n")}")
+    def have_valid_billing_address(credit_card)
+      if self.class.validate_billing_address?(credit_card)
+        credit_card.instance_eval do
+          errors.add(:billing_address, "is empty.") and return if billing_address.blank?
+          errors.add(:billing_address, "is not valid. #{billing_address.errors.full_messages.join("\n")}") if billing_address.invalid?
+        end
       end
     end
   end
