2ff91ace13b342adfba8c774e997aeb7bd4e3b40
Lin Yang <github@linyang.me>
2013-04-05 21:23:12 -0700

    Make all classes eager loadable

--- lib/braintree_rails/braintree_rails.rb
+++ lib/braintree_rails/braintree_rails.rb
@@ -1,8 +1,10 @@
 module BraintreeRails
   extend ActiveSupport::Autoload
   Dir.chdir(File.dirname(__FILE__)) do
-    Dir.glob("*.rb").each do |file|
-      autoload file.chomp(".rb").camelize.to_sym
+    eager_autoload do
+      Dir.glob("*.rb").each do |file|
+        autoload file.chomp(".rb").camelize.to_sym
+      end
     end
   end
 end
