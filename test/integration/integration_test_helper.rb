6f7dc28eda72d004cfb0bec0490d2bd54fecf43b
Lin Yang <github@linyang.me>
2014-03-07 12:27:36 -0800

    Try to run integrations test in Travis CI

--- test/integration/integration_test_helper.rb
+++ test/integration/integration_test_helper.rb
@@ -7,6 +7,12 @@ if File.exist?(config) && auth = YAML.load_file(config)
   BraintreeRails::Configuration.private_key = auth['private_key']
   BraintreeRails::Configuration.default_merchant_account_id = auth['default_merchant_account_id']
   # BraintreeRails::Configuration.logger = Logger.new('log/braintree.log')
+elsif ENV["TRAVIS_SECURE_ENV_VARS"] == "true"
+  BraintreeRails::Configuration.environment = :sandbox
+  BraintreeRails::Configuration.merchant_id = ENV['merchant_id']
+  BraintreeRails::Configuration.public_key = ENV['public_key']
+  BraintreeRails::Configuration.private_key = ENV['private_key']
+  BraintreeRails::Configuration.default_merchant_account_id = ENV['default_merchant_account_id']
 else
   puts '*' * 80
   puts "You need to provide real credentials in #{config} to run integration tests"
